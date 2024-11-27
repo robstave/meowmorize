8fa4734b17f522c38d16af3e8679bf7456fa8931
Rob STave <robstave@yahoo.com>
2024-11-27 12:16:03 -0600

    cleanup

--- older-project/internal/domain/types/deck.go
+++
@@ -1,9 +0,0 @@
-package types
-
-type Deck struct {
-	ID          string `gorm:"primaryKey" json:"id"` // UUID string as the primary key
-	Name        string `gorm:"type:varchar(100);not null" json:"name"`
-	Cards       []Card `gorm:"foreignKey:DeckID" json:"cards"`
-	Description string `gorm:"type:text" json:"description"` // New Description field
-
-}
