f1bb7eda4e073817bc809130d611ed8bea9eef77
Rob STave <robstave@yahoo.com>
2025-02-17 00:44:27 -0600

    :Squashed commit of the following:
    commit 3375ae6fc4f40d56c20e5f3797bd9367a2938b5e
    Author: Rob STave <robstave@yahoo.com>
    Date:   Sat Feb 15 10:57:55 2025 -0600
    
        added password to docker build
    
    commit 4be0c11dc2101721662236bc4fdad92ac79619de
    Author: Rob STave <robstave@yahoo.com>
    Date:   Sun Feb 9 12:18:29 2025 -0600
    
        tweek yaml

--- internal/domain/seeder.go
+++ internal/domain/seeder.go
@@ -2,25 +2,37 @@
 package domain
 
 import (
-	"golang.org/x/crypto/bcrypt"
+	"os"
 
 	"github.com/google/uuid"
 	"github.com/robstave/meowmorize/internal/domain/types"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func (s *Service) SeedUser() error {
+	// Read default username and password from environment variables.
+	// If not provided, default to "meow"
+	defaultUsername := os.Getenv("DEFAULT_USER_USERNAME")
+	if defaultUsername == "" {
+		defaultUsername = "meow"
+	}
+	defaultPassword := os.Getenv("DEFAULT_USER_PASSWORD")
+	if defaultPassword == "" {
+		defaultPassword = "meow"
+	}
+
 	// Check if the user already exists
-	existingUser, err := s.userRepo.GetUserByUsername("meow")
+	existingUser, err := s.userRepo.GetUserByUsername(defaultUsername)
 	if err != nil {
 		return err
 	}
 	if existingUser != nil {
-		// User already exists
+		// User already exists; no need to seed
 		return nil
 	}
 
 	// Hash the password
-	hashedPassword, err := bcrypt.GenerateFromPassword([]byte("meow"), bcrypt.DefaultCost)
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(defaultPassword), bcrypt.DefaultCost)
 	if err != nil {
 		return err
 	}
@@ -28,7 +40,7 @@ func (s *Service) SeedUser() error {
 	// Create the user
 	user := types.User{
 		ID:       uuid.New().String(),
-		Username: "meow",
+		Username: defaultUsername,
 		Password: string(hashedPassword),
 	}
 
